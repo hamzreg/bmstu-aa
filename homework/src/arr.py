f6b2ce6fbec322f7879844be506a6ec284f81b5f
khrr19u863 <regina.hamsina@gmail.com>
2021-11-04 01:01:17 +0300

    FIHISH: homework

--- homework/src/arr.py
+++ homework/src/arr.py
@@ -1,58 +1,40 @@
-from dataclasses import dataclass
 from random import randint
 
-@dataclass
-class ARR:
-    """
-        Константы для массива.
-    """
-
-    low = -10
-    top = 10
-
-
-def counting_sort(arr, size):
+def counting_sort():
     """
         Сортировка подсчетом.
     """
-
-    max_elem = arr[0]                       # 1
-    min_elem = max_elem                     # 2
-
-    for i in range(1, size):                # 3
-        if max_elem < arr[i]:               # 4
-            max_elem = arr[i]               # 5
-        if min_elem > arr[i]:               # 6
-            min_elem = arr[i]               # 7
     
-    d = min_elem - 1                        # 8
-    add_size = max_elem - min_elem + 1      # 9
+    size = int(input("Input size: "))       # 1
 
-    add_arr = [0] * add_size                # 10
+    arr = []                                # 2
 
-    for i in range(size):                   # 11
-        j = arr[i] - d - 1                  # 12
-        add_arr[j] += 1                     # 13
+    for _ in range(size):                   # 3
+        arr.append(randint(-10, 10))        # 4
+
+    min_elem = min(arr)                     # 5
+    max_elem = max(arr)                     # 6
+    
+    d = min_elem - 1                        # 7
+    add_size = max_elem - min_elem + 1      # 8
+
+    add_arr = [0] * add_size                # 9
+
+    for i in range(size):                   # 10
+        j = arr[i] - d - 1                  # 11
+        add_arr[j] += 1                     # 12
     
     i = 0                                   # 13
 
-    for j in range(add_size):               # 15
-        if add_arr[j] > 0:                  # 16
-            for _ in range(add_arr[j]):     # 17
-                arr[i] = j + d              # 18
-                i += 1                      # 19
+    for j in range(add_size):               # 14
+        if add_arr[j] > 0:                  # 15
+            for _ in range(add_arr[j]):     # 16
+                arr[i] = j + d              # 17
+                i += 1                      # 18
 
     return arr
 
 
-def get_random_arr(size):
-    """
-        Случайный массив.
-    """
-
-    arr = []
-
-    for _ in range(size):
-        arr.append(randint(ARR.low, ARR.top))
-
-    return arr
+if __name__ == "__main__":
+    arr = counting_sort()
+    print("Отсортированный массив: ", arr)
--- homework/src/main.py
+++
@@ -1,9 +0,0 @@
-from arr import get_random_arr, counting_sort
-
-
-if __name__ == "__main__":
-    size = int(input("Введите размер массива: "))
-    arr = get_random_arr(size)
-    print("Массив для сортировки: ", arr)
-    arr = counting_sort(arr, len(arr))
-    print("Отсортированный массив: ", arr)
